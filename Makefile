aa8f8486dc845506df8b95a1bdc3cac1ac8a6ffb
shushu <golpos369@gmail.com>
2021-10-17 23:51:50 -0400

    done ?

--- Makefile
+++ Makefile
@@ -1,2 +1,11 @@
+all: main.o
+	gcc -o list main.o
+
+main.o: main.c
+	gcc -c main.c
+
 clean:
 	rm *.o *.exe
+
+run:
+	./list
--- main.c
+++ main.c
@@ -1,28 +1,62 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-struct national_dex
+#include <string.h>
+#include <time.h>
+struct dex
 {
     char *name;
     int id;
-    struct national_dex *next;
+    struct dex *next;
 };
+
+void print_list(struct dex *pokemon);
+struct dex *insert_front(struct dex *pokemon, char *name, int id);
+struct dex *free_list(struct dex *front);
 
 int main(void)
 {
+    struct dex *pokemon;
+
+    pokemon = insert_front(pokemon, "Pikachu", 25);
+    pokemon = insert_front(pokemon, "Arceus", 493);
+    pokemon = insert_front(pokemon, "Weavile", 461);
+    pokemon = insert_front(pokemon, "Chandelure", 609);
+    pokemon = insert_front(pokemon, "Mew", 151);
+
+    print_list(pokemon);
+    pokemon = free_list(pokemon);
 }
 
-void print_list(struct national_dex *pokemon)
+void print_list(struct dex *pokemon)
 {
-    print("%s's national dex number is %d", pokemon->name, pokemon->id);
+    struct dex *node = pokemon;
+    while (node != NULL)
+    {
+        printf("%s's national dex number is %d.\n", node->name, node->id);
+        node = node->next;
+    }
 }
 
-struct national_dex *insert_front(struct national_dex *pokemon, char *name, int id)
+struct dex *insert_front(struct dex *pokemon, char *name, int id)
 {
-    struct national_dex *new_pokemon = malloc(sizeof(struct national_dex));
-    new_pokemon->name = name;
+    struct dex *new_pokemon = malloc(sizeof(struct dex));
+    new_pokemon->name = malloc(strlen(name) + 1);
+    strcpy(new_pokemon->name, name);
     new_pokemon->id = id;
     new_pokemon->next = pokemon;
 
     return new_pokemon;
+}
+
+struct dex *free_list(struct dex *front)
+{
+    struct dex *node = front;
+    while (node != NULL)
+    {
+        struct dex *t = node;
+        node = node->next;
+        free(t);
+    }
+
+    return front;
 }
