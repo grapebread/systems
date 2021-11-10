c2f419e653a6f9bb51ab5037669a1e6aea1dbdfa
shushu <golpos369@gmail.com>
2021-11-09 20:22:35 -0500

    should be complete

--- main.c
+++ main.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <signal.h>
-#include <process.h>
-#include <windows.h>
-#include <io.h>
+#include <string.h>
+#include <unistd.h>
+#include <errno.h>
 #include <fcntl.h>
+#include <signal.h>
 
 #define TRUE 1
 
@@ -13,6 +13,11 @@ static void sighandler(int signo)
     if (signo == SIGINT)
     {
         int file = open("signal.txt", O_WRONLY | O_APPEND | O_CREAT, 0777);
+        if (errno != 0)
+        {
+            printf("Error: %s\n", strerror(errno));
+        }
+
         char s[9] = "Written.\n";
         write(file, s, sizeof(s));
         close(file);
@@ -20,7 +25,7 @@ static void sighandler(int signo)
     }
     else if (signo == SIGUSR1)
     {
-        printf("%s\n", getppid());
+        printf("ppid: %s\n", getppid());
     }
 }
 
@@ -30,6 +35,6 @@ int main(void)
     {
         signal(SIGINT, sighandler);
         sleep(1);
-        printf("%d\n", getpid());
+        printf("pid: %d\n", getpid());
     }
 }
