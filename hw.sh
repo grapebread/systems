c8c764abf7c4c8ab48b398e5e1b3853c34edfbec
Shuprovo Sikder <54008448+meowzebub@users.noreply.github.com>
2021-12-10 13:20:38 -0500

    Update hw.sh

--- hw.sh
+++ hw.sh
@@ -4,7 +4,7 @@ git clone git@github.com:mks65/"$1".git
 cd ./"$1"/4
 git submodule add -b "$1" git@github.com:meowzebub/systems.git sikderShu
 git add .
-git commit -S -m "added submodule"
+git commit -m "added submodule"
 git push
 cd ../../
 rm -rf "$1"
